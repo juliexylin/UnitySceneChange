c7033ffdd51681c8147d91e0deeb2191a7d95f20
Myungin Lee <benedic.lee@gmail.com>
2024-03-12 01:19:26 -0400

    Init

---
+++ Assets/Scripts/CollideTrigger.cs
@@ -0,0 +1,40 @@
+// Tutorial for changing scenes
+// Documentatin: https://docs.unity3d.com/ScriptReference/SceneManagement.SceneManager.html
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollideTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if collision is made with the Drop, which is tagged as "Finish"
+        // You can change the tag and manually change this accordingly.
+        if (other.tag == "LevelExit")
+        {
+            // Print out the current scene's name
+            Debug.Log(SceneManager.GetActiveScene().name);
+            // Change scene
+            SceneManager.LoadScene("01_Next");
+            
+            // This will do the same thing.
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+
+}
