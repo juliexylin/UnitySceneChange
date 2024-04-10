6206477a54f6f8ac77c7ec49ca302e2354f3a70e
Myungin Lee <benedic.lee@gmail.com>
2024-04-10 19:54:56 -0400

    Clone and Chase added

--- Assets/Scripts/CollideTrigger.cs
+++ Assets/Scripts/CollideTrigger.cs
@@ -8,18 +8,6 @@ using UnityEngine.SceneManagement;
 
 public class CollideTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         // Check if collision is made with the Drop, which is tagged as "Finish"
@@ -35,6 +23,4 @@ public class CollideTrigger : MonoBehaviour
             //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
-
-
 }
